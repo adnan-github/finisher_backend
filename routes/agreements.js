c1410a7cc3e24679c5241ba57634e2062f1a21cd
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2019-03-13 05:51:47 +0500

    fixed formatting

--- routes/agreements.js
+++ routes/agreements.js
@@ -16,20 +16,24 @@ agreementsRouter.post('/initiate', (req, res, next) => {
 
   var io = req.app.io;
   var nearByProviders = req.body.nearByProviders;
-  console.log(req.body);
-  customers_Location(req.body.customer_id).then(data => {
+
+  customers_Location(req.body.customer_id).then( data => {
+
+    // this object will be sent with socket action to the provider
     let customer_object = {
-      name: data.customerId.name,
-      address: data.address,
-      phone: data.customerId.phone,
-      agreement_type: req.body.agreement_type
+          name            : data.customerId.name,
+          address         : data.address,
+          phone           : data.customerId.phone,
+          agreement_type  : req.body.agreement_type
     };
+
+    // initiating the agreement
     agreementsModel.create(new agreementsModel({
-      customer_id : req.body.customer_id,
-      selected_service: req.body.selected_service,
-      status: 'pending',
-      agreement_type: req.body.agreement_type,
-      socketId: data.socketId
+        customer_id       : req.body.customer_id,
+        selected_service  : req.body.selected_service,
+        status            : 'pending',
+        agreement_type    : req.body.agreement_type,
+        socketId          : data.socketId
     }), (err, data ) => {
       if ( err ){
         res.statusCode = 500;
@@ -39,34 +43,12 @@ agreementsRouter.post('/initiate', (req, res, next) => {
       else {
         res.json({ success: true, data});
         nearByProviders.sockets.forEach( socketId => {
-            io.to(socketId).emit('action', { type: 'SERVICE_REQUEST', data: customer_object});
+            io.to(socketId).emit('action', { type: 'SERVICE_AGREEMENT_REQUEST', data: customer_object });
         });
       }
-    })
+    }); // agreement creation request ends here 
+
   }).catch(err => res.json(err ));
-  // agreementsModel.create(new agreementsModel({
-  //   customer_id       : req.body.customer_id,
-  //   selected_service  : req.body.selected_service,
-  //   status            : "pending",
-  //   agreement_type    : req.body.agreement_type
-  // }),(err, agreement) => {
-  //   if (err) {
-  //     res.statusCode = 500;
-  //     res.setHeader('Content-Type', 'application/json');
-  //     res.json({err: err});
-  //   } else {        
-  //     nearByProviders.forEach(provider => {
-  //       if( provider.socketId ){
-  //         let driver_channel = provider.socketId + 'provider_request';
-  //         io.emit( driver_channel , nearByProviders);
-  //       }
-        
-  //     });
-  //     res.statusCode = 200;
-  //     res.setHeader('Content-Type', 'application/json');
-  //     res.json({ success: true, status: 'agreement created successfully', agreement: agreement});
-  //   }
-  // });
 });
 
 // agreements route for delete service
@@ -84,7 +66,7 @@ agreementsRouter.delete('/remove', (req, res, next) => {
 });
 
 
-// agreements route for get all service
+// agreements route to get all service
 agreementsRouter.get('/all', (req, res, next) => {
 
   agreementsModel.find({},(err, agreements) => {
